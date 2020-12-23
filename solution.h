ffa3af7dfa6885deaf7a314e50998e1b956bea85
Антон Шмидт <schmidt.ajax@gmail.com>
2020-12-23 12:52:17 +0700

    Sort original vector

--- solution.h
+++ solution.h
@@ -1,9 +1,9 @@
 #ifndef SOLUTION_H
 #define SOLUTION_H
 
+#include <algorithm>
 #include <iostream>
 #include <numeric>
-#include <set>
 #include <vector>
 
 using namespace std;
@@ -11,13 +11,13 @@ using namespace std;
 class Solution {
 public:
   int smallestRangeII(vector<int>& A, int K) {
-    set<int> numbers(A.begin(), A.end());
-    int amin = *numbers.begin();
-    int amax = *prev(numbers.end());
+    sort(A.begin(), A.end());
+    int amin = A.front();
+    int amax = A.back();
     int smallest_distnace = abs(amax - amin);
-    for (auto iter = numbers.begin(); next(iter) != numbers.end(); ++iter) {
-      int smaller = *iter;
-      int greater = *next(iter);
+    for (size_t i = 0; i + 1 < A.size(); ++i) {
+      int smaller = A[i];
+      int greater = A[i + 1];
       int low = min(amin + K, greater - K);
       int high = max(amax - K, smaller + K);
       smallest_distnace = min(smallest_distnace, abs(high - low));
